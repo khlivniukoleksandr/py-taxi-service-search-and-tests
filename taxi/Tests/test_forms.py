134d50d5320b0d247121fdb28534705e6766f76d
Khlivniuk Oleksandr <sasha2056083@gmail.com>
2025-06-27 13:52:02 +0300

    Add Tests Creation Driver and Update Driver

--- taxi/Tests/test_forms.py
+++ taxi/Tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from taxi.forms import DriverCreationForm
+from taxi.forms import DriverCreationForm, DriverLicenseUpdateForm
+from taxi.models import Driver
 
 
 class FormTests(TestCase):
@@ -16,3 +17,75 @@ class FormTests(TestCase):
         form = DriverCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data["username"], form_data["username"])
+
+
+class DriverLicenseUpdateFormTest(TestCase):
+    def setUp(self):
+        self.driver = Driver.objects.create_user(
+            username="John_Doe",
+            password="test1234",
+            license_number="XYZ12345",
+            first_name="John",
+            last_name="Doe"
+            )
+
+    def test_update_valid_license_number(self):
+        form = DriverLicenseUpdateForm(data={"license_number": "ABC12345"},
+                                       instance=self.driver)
+        self.assertTrue(form.is_valid())
+        driver = form.save()
+        self.assertEqual(driver.license_number, "ABC12345")
+
+    def test_update_short_license_number(self):
+        form = DriverLicenseUpdateForm(data={"license_number": "abc1234"},
+                                       instance=self.driver)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
+    def test_first_digits_not_letters_and_not_capitalized(self):
+        form = DriverLicenseUpdateForm(data={"license_number": "a2C12345"},
+                                       instance=self.driver)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
+    def test_last_five_digits_not_a_numbers(self):
+        form = DriverLicenseUpdateForm(data={"license_number": "ABC123fg"},
+                                       instance=self.driver)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
+
+class DriverCreationLicenseNumberFormTest(TestCase):
+    def setUp(self):
+        self.data = {
+            "username": "new_driver",
+            "password1": "StrongPass123",
+            "password2": "StrongPass123",
+            "first_name": "Alice",
+            "last_name": "Smith"
+        }
+    def test_create_valid_license_number(self):
+        data = {**self.data, "license_number": "ABC12345"}
+        form = DriverCreationForm(data=data)
+        self.assertTrue(form.is_valid())
+        driver = form.save()
+        self.assertEqual(driver.license_number, "ABC12345")
+
+    def test_create_short_license_number(self):
+        data = {**self.data, "license_number": "ABC12"}
+        form = DriverCreationForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
+    def test_first_digits_not_letters_and_not_capitalized(self):
+        data = {**self.data, "license_number": "a2C12345"}
+        form = DriverCreationForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
+    def test_last_five_digits_not_a_numbers(self):
+        data = {**self.data, "license_number": "ABC123fg"}
+        form = DriverCreationForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
--- taxi/Tests/test_views.py
+++ taxi/Tests/test_views.py
@@ -52,3 +52,5 @@ class DriverSearchTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "John")
         self.assertNotContains(response, "Alex_Black")
+
+
